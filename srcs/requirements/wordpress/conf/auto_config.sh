4c0b3077c82f44624e84287d2ad2be05647bbd92
hiabed <abed.hassani.idrissi@gmail.com>
2024-02-05 16:36:52 +0100

    handle some small errors

--- srcs/requirements/wordpress/conf/auto_config.sh
+++ srcs/requirements/wordpress/conf/auto_config.sh
@@ -5,11 +5,11 @@ cd /var/www/wordpress
 sleep 5
 
 wp config create --allow-root \
-    --dbname=$SQL_DATABASE \
-    --dbuser=$SQL_USER \
-    --dbpass=$SQL_PASSWORD \
-    --dbhost=mariadb:3306 \
-    --path='/var/www/wordpress'
+  --dbname=$SQL_DATABASE \
+  --dbuser=$SQL_USER \
+  --dbpass=$SQL_PASSWORD \
+  --dbhost=mariadb:3306 \
+  --path='/var/www/wordpress'
 
 wp core install --allow-root \
   --url="$WP_URL" \
@@ -18,6 +18,9 @@ wp core install --allow-root \
   --admin_password="$WP_ADM_PASSWORD" \
   --admin_email="$WP_ADM_EMAIL"
 
-wp user create --allow-root "$WP_USER" --user_pass="$WP_USER_PASSWORD" --role="$author" --path='/var/www/wordpress'
+wp user create --allow-root \
+  "$WP_USER" "$WP_USER_EMAIL" \
+  --user_pass="$WP_USER_PASSWORD" \
+  --role="$author" --path='/var/www/wordpress'
 
 /usr/sbin/php-fpm7.3 -F
